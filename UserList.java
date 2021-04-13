d32c0cf4a0801d56222d955a317a4e591a414827
Romuald80 <74305711+Romuald80@users.noreply.github.com>
2021-04-13 23:51:22 +0200

    Update UserList.java

--- UserList.java
+++ UserList.java
@@ -1,4 +1,36 @@
 import java.sql.*;
 public class UserList {
+    package com.company;
+
+    public class Uzytkownik {
+        private String imie;
+        private String nazwisko;
+        private int id;
+
+        public String getImie() {
+            return imie;
+        }
+
+        public void setImie(String imie) {
+            this.imie = imie;
+        }
+
+        public String getNazwisko() {
+            return nazwisko;
+        }
+
+        public void setNazwisko(String nazwisko) {
+            this.nazwisko = nazwisko;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
+
 
 }
